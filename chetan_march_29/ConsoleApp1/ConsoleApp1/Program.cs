45d4e516667d41fdda107b7af152aec15f344e10
Chetan Rahanoo <732135@student.tor.loc>
2019-03-29 20:05:47 -0400

    first commit

---
+++ chetan_march_29/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			HelloWorld hw=new HelloWorld();
+			hw.Hello();
+        }
+		public class HelloWorld{
+			
+			
+			public void Hello(){
+				Console.WriteLine("Hello world");
+				
+			}
+		}
+    }
+}
